e5c6d7d58be38f555dfaa3d333eeb08382fe23dc
jguerraa95 <jguerraa95@gmail.com>
2025-03-29 18:34:11 -0500

    Primer commit

---
+++ app/src/main/java/com/jorgeguerra/fakestoremvvm/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.jorgeguerra.fakestoremvvm.di
+
+import com.jorgeguerra.fakestoremvvm.store.data.remote.ProductsApi
+import com.jorgeguerra.fakestoremvvm.util.Constant.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideProductsApi(): ProductsApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ProductsApi::class.java)
+    }
+
+}
